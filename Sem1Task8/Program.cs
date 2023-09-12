932120b8c3fe2c18110c5e98cb9b2d9b5e391d90
Andrei <ershov-andrei.93@mail.ru>
2023-09-12 16:12:30 +0500

    solution from sem1 task 8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,19 +1,13 @@
 ﻿Console.WriteLine("Введите число:");
-int num = Convert.ToInt32(Console.ReadLine()??"0");
+int num = int.Parse(Console.ReadLine()??"0");
 int count = 0;
-if (num % 2 == 0)
-{
-    while (count < num)
-    {
-        count = count + 2;
-        Console.WriteLine(count);
-    }
-}
 
 if (num % 2 != 0)
 {
     num = num - 1;
-    while (count < num)
+}
+while (count < num)
+{
     {
         count = count + 2;
         Console.WriteLine(count);
