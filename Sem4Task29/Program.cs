93e9a7ace8440945a8b8fc969a130054de2460db
Andrei <ershov-andrei.93@mail.ru>
2023-09-28 16:08:10 +0500

    sem4task29

--- Sem4Task29/Program.cs
+++ Sem4Task29/Program.cs
@@ -1,80 +1,14 @@
-﻿Console.Write("Введите ряд чисел, разделенных запятой : ");
-string? seriesOfNumbers = Console.ReadLine();
-
-
-string RemovingSpaces (string series){
-string seriesNew = " ";
-for (int i = 0; i < series.Length; i++)
+﻿int ReadData(string msg)
 {
-    if (series[i] != ' ') 
-    {
-    seriesNew += series[i];
-    }
-}
-    return seriesNew;
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
 }
 
-//  функция  проверки на правильность ввода 
-void СheckNumber2 (int  series)
+int[] Arrey = new int[8];
+Console.Write("");
+for (int i = 0; i < Arrey.Length; i++)
 {
-    if (series == '0'||series == '1'||series == '2'
-    ||series == '3'||series == '4'||series == '5'||series == '6'
-    ||series == '7'||series == '8'||series == '9'||series == ','
-    ||series == '-')
-    {
-    }
-        else {
-        Console.WriteLine($"Ошибка ввода  символа. Вводи цифры.");
-
-    }
+    Arrey[i] = new Random().Next(0, 9);
+    Console.Write(Arrey[i]);
 }
-
-// функция  создания и заполнения массива из строки
-int[] ArrayOfNumbers(string seriesNew)
-{ 
-
-    int[] arrayOfNumbers = new int[1];    // инициализация массива из 1 элемента
-
-    int j =0;
-
-    for (int i = 0; i < seriesNew.Length; i++)
-    {
-    string seriesNew1 = "";
-
-    while (seriesNew[i] != ',' && i < seriesNew.Length){
-    seriesNew1 += seriesNew[i];
-    СheckNumber2(seriesNew[i]);
-    i++;
-    }
-    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    // заполняет массив значениями из строки
-    if (i < seriesNew.Length-1){
-      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    // добавляет новый нулевой элемент в конец массива
-    }
-    j++;
-    }
-    return arrayOfNumbers;
-}
-
-// функция  вывода массива на печать 
-void PrintArry(int[] coll)
-{
-    int count = coll.Length;
-    int index = 0;
-    Console.Write("[");
-    while(index < count)
-    {
-    Console.Write(coll[index]);
-    index++;
-    if (index < count){
-    Console.Write(", ");
-    }
-    }
-    Console.Write("]");
-} 
-
-
-string seriesNew = RemovingSpaces(seriesOfNumbers);
-
-int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
-
-PrintArry(arrayOfNumbers);
