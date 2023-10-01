59398ee5e9ada8953bd9728c8341cbeb4746ba7c
Andrei <ershov-andrei.93@mail.ru>
2023-10-01 18:07:09 +0500

    solution from Sem5Task 31,32,33,34,35,36,37,38

---
+++ Sem5Task31/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+int[] GenArr(int len, int lowBorder, int highBorder) 
+
+
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder+1);
+    }
+    return arr;
+}
+
+(int, int) NegotivPositivSum(int[]arr)
+{
+    int posSum = 0;
+    int negSum = 0;
+
+    for(int i = 0; i < arr.Length;i++)
+    {
+        if(arr[i] > 0)
+        {
+            posSum=posSum+arr[i];
+        }
+        else
+        {
+            negSum+=arr[i];
+        }
+    }
+    return(posSum, negSum);
+}
+
+void  PrintArr(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0; i<arr.Length-1;i++)
+    {
+        Console.Write(arr [i]+",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int[] arr = GenArr(12, -9, 9);
+PrintArr(arr);
+(int pos, int neg) sum = NegotivPositivSum(arr);
+Console.WriteLine("Сумма > 0: "+sum.pos+" Сумма < 0: " +sum.neg);
---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача №32
+// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] GenArr(int len, int lowBorder, int highBorder) 
+
+
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(lowBorder, highBorder+1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+    System.Console.WriteLine();
+}
+
+void InversArray(int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        arr[i]*=-1;
+    }
+}
+
+int[] array=GenArr(12,-9,9);
+PrintArray(array);
+InversArray(array);
+PrintArray(array);
