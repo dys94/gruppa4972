331310f586aa61a7f3bb819f06079648cf29f71b
Andrei <ershov-andrei.93@mail.ru>
2023-10-06 16:21:12 +0500

    solution from sem7task 46 47 47 49 50 51

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Ввод парметров матрицы.
+int countRow = ReadInt("Введите количество строк m: ");
+int countColumn = ReadInt("Введите количество cтолбцов n: ");
+
+// Генерация и вывод матрицы.
+int minValue = -10;
+int maxValue = 10;
+double[,] matrix = CreateRandomMatrix(countRow, countColumn, minValue, maxValue);
+PrintMatrix(matrix);
+
+
+// Ввод целого числа с клавиатуры.
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генераци двумерного массива, заполненного случайными вещественными значениями.
+double[,] CreateRandomMatrix(int countRow, int countColumn, int minValue, int maxValue)
+{
+    Random random = new Random();
+    double[,] matrix = new double[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            matrix[i, j] = (maxValue - minValue) * random.NextDouble() + minValue;
+        }
+    }
+    return matrix;
+}
+// Вывод двумерного массива на экран.
---
+++ Sem7Task52/Program.cs
@@ -0,0 +1,30 @@
+﻿int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+// Заполнение случайными числами
+Random input = new Random();
+int[,] arr = new int[input.Next(1, 10), input.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++) // Вывод элементов
+    {
+        arr[i, j] = input.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
