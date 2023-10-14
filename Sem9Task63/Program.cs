85d8409720fc5c906025f6133e5ca6d5940d4621
Andrei <ershov-andrei.93@mail.ru>
2023-10-14 18:42:38 +0500

    solution from Sem9task 63 64 65 66 67 68 69

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine()?? "0");
+    return number;
+}
+
+// N=5 -> 5 4 3 2 1
+void GenLineRec(int num)
+{
+    Console.WriteLine(" "+num);
+    if(num<=1)
+    {
+    }
+    else
+    {
+    GenLineRec(num - 1);
+    }
+}
+
+int num = ReadData("Введите число N: ");
+GenLineRec(num);
---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadData(string msg) // вводим данные
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+int digitSum(int num)
+{
+
+    if(num > 0)
+    {
+    return  digitSum(num/10)+num%10;
+    } else return 0;
+
+}
+
+int num = ReadData("Введите число");
+Console.WriteLine($"Сумма цифр { digitSum(num)}");
